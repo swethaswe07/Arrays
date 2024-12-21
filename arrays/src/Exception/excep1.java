e4515ca5f4ad4a12458976a4be5d21cfbd1906c6
Maniswethu <Maniswethu@MANI_SWETHA.lan>
2024-12-21 08:16:53 +0530

    initial push

--- arrays/src/Exception/excep1.java
+++ arrays/src/Exception/excep1.java
@@ -1,11 +1,9 @@
 package Exception;
 
 public class excep1 {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws CustomExcep {
 		try {
 			throw  new CustomExcep("New Exception");
-			throw new CustomExcep("New Exception");
-			throw new CustomExcep("New Exception");
 			
 			
 		}
@@ -13,15 +11,5 @@ public class excep1 {
 			System.out.println("exception caught");
 			System.out.println(ex.getMessage());
 		}
-		catch(CustomExcep ex1) {
-			System.out.println("Exception 1 caught");
-			System.out.println(ex1.getMessage());
-		
-		}
-		catch(CustomExcep ex2) {
-			System.out.println("Exception 2 caught");
-			System.out.println(ex2.getMessage());
-		}
 	}
-}
 }
